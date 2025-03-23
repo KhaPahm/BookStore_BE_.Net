e9d8a8633e887daf81fba11f450ea72037973f00
KhaPahm <khapham.work@gmail.com>
2025-03-24 00:25:45 +0700

    feat: add static models

---
+++ Static/AuthProvider.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Static
+{
+    public static class AuthProvider
+    {
+        public const string Google = "GOOGLE";
+        public const string Local = "LOCAL";
+        public const string Facebook = "FACEBOOK";
+    }
+}
---
+++ Static/UserRole.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Static
+{
+    public class UserRole
+    {
+        public const string Admin = "ADMIN";
+        public const string Staff = "STAFF";
+        public const string Customer = "CUSTOMER";
+    }
+}
